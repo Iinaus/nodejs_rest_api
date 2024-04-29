434fba149eadb51f3ab2af969165a4d658f32fe1
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-04-29 10:23:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Dependencies
+
+This project relies on the following dependencies:
+
+- **sqlite3**: To interact with SQLite database.
+```bash
+npm i sqlite3
+```
+
+
+- **express**: To handle HTTP requests and routes.
+```bash
+npm install express --save
+```
+
+
+Make sure to install these dependencies before running the project to ensure smooth execution.
---
+++ public/app.js
@@ -0,0 +1,46 @@
+async function getUsers() {
+    try {
+        const response = await fetch("http://localhost:3000/api/v1/user")
+
+        const data = await response.json()
+
+        console.log(data)
+
+    } catch (error){
+        console.log(error)
+    }    
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+
+    await getUsers()
+
+    const button = document.createElement('button')
+
+    button.innerText = "Luo käyttäjä"
+
+    button.addEventListener('click', async () => {
+        const response = await fetch("http://localhost:3000/api/v1/user", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: 'testi' + crypto.randomUUID(),
+                password: 'salasana',
+                age: 50,
+                role: 'user'
+            })
+        })
+
+        if(!response.ok){
+            alert("Käyttäjän luonti epäonnistui.")
+            return
+        }
+
+        alert("Käyttäjä luotu onnistuneesti.")
+    })
+
+    document.body.append(button)
+
+})
